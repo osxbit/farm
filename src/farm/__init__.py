f63fedde0bed48826bb1e3f58dc0a43379ed521d
Baciacia <secondip01@yahoo.com>
2023-11-07 23:27:36 +0200

    real-time sending of video stream

--- src/farm/__init__.py
+++ src/farm/__init__.py
@@ -1,3 +1,8 @@
+import cv2
+import base64
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 class VideoStream:
     def stream(self):
         pass
@@ -26,3 +31,25 @@ rtspStream.stream()
 
 httpStream = stream_factory.create_stream("HTTP")
 httpStream.stream()
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+camera = cv2.VideoCapture(0)
+
+@socketio.on('image')
+
+def send_image():
+    while True:
+        success, frame = camera.read()
+        if success:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_encoded = base64.b64encode(buffer).decode('utf-8')
+            socketio.emit('image_response', frame_encoded)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    socketio.run(app)
