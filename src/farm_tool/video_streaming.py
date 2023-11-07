c03f7c95f40db4bc52cbcb42d4aaacfc0013c948
Baciacia <secondip01@yahoo.com>
2023-11-07 23:04:10 +0200

    real-time sending of video stream

--- src/farm_tool/video_streaming.py
+++ src/farm_tool/video_streaming.py
@@ -1,4 +1,8 @@
-#cred ca v ati prins oricum ce e aici, am facut o clasa factory care creeaza diferite tipuri de instante in functie de tipul dat
+
+import cv2
+import base64
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 class VideoStream:
     def stream(self):
@@ -28,4 +32,26 @@ rtspStream.stream()
 
 httpStream = stream_factory.create_stream("HTTP")
 httpStream.stream()
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+camera = cv2.VideoCapture(0)
+
+@socketio.on('image')
+
+def send_image():
+    while True:
+        success, frame = camera.read()
+        if success:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_encoded = base64.b64encode(buffer).decode('utf-8')
+            socketio.emit('image_response', frame_encoded)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    socketio.run(app)
 
